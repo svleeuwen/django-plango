b58b6bb069bbf99ad20cd764c748e41e60d274d5
elwin schmitz <ik@elwin.nu>
2012-02-20 15:06:23 +0100

    Grote Front-End Schoonmaak:
    + base.html + _template_page.html aangevuld vanuit HTML5-Boilerplate v3
    + CSS + images folders herordend
      + CSS bestanden opgedeeld in meerdere losse
      + base.css aangevuld vanuit H5BP v3
    + Presentatie-images en CMS-Placeholder-images in twee losse mappen
    +
    + jQuery + Modernizr versies geupdate
    + *plugins.js: Enkele losse javascript code-snippets aan toegevoegd, om vanuit te cut&pasten, niet voor productie
    + Nu 1 locatie voor scripts, alleen in /static/website/behaviour/

--- website/static/website/behaviour/engine.js
+++ website/static/website/behaviour/engine.js
@@ -1,1 +1,64 @@
-/*** * Fabrique - Skeleton * * Description: Generic userinterface enhancements * Author: Fabrique[brands, design &amp; interaction] - http://fabrique.nl/           MartijnG, Anna & Dodi ... <insert Fabriqueprojectmembers here> * Date: 2011-03-18 *//*** * Application start - public void main */function Application() {}Application.prototype = {  init: function() {    this.setupPrint();    this.setupExternalLinks();  },  setupPrint: function() {    $('a.print').click(function(e) {      print();      e.preventDefault();      e.stopPropagation();    });  },  setupExternalLinks: function() {    $("a[rel='external']").each(function(index, el) {      //...    });  }};var app = new Application();$(document).ready(function() {  app.init();});
+/**
+	Example.nl - engine.js
+*/
+"use strict";
+
+/* TODO: load dependent scripts here (Modernizr.load?) */
+
+
+/**
+	Setup / Config
+*/
+
+
+
+/**
+	Base:
+*/
+
+var Example = {
+
+	settings: {},
+
+	defaults: {},
+
+	init: function () {
+
+//		console.log('init');
+
+	},
+
+	doSomething: function (event) {
+
+	}
+
+};
+
+
+
+
+
+/**
+	Document Ready!
+	Move all setup/init function-calls to this section, to run once the document is ready.
+*/
+$(document).ready(function () {
+
+
+
+
+
+
+/**
+	Skip-to-Main links move focus to #main:
+*/
+	// add a click handler to all links that point to same-page targets (href="#...")
+	$("a[href^='#']").click(function () {
+		// get the href attribute of the internal link
+		// then strip the first character off it (#)
+		// leaving the corresponding id attribute
+		$("#" + $(this).attr("href").slice(1)).focus();
+	});
+
+
+}); // end document.ready
--- website/static/website/behaviour/plugins.js
+++ website/static/website/behaviour/plugins.js
@@ -1,9 +1,89 @@
-window.log = function(){
-  log.history = log.history || [];  
-  log.history.push(arguments);
-  arguments.callee = arguments.callee.caller;  
-  if(this.console) console.log( Array.prototype.slice.call(arguments) );
-};
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
+/**
+	Helper functions:
+*/
+"use strict";
 
+
+/**
+	Arrays:
+*/
+
+Array.prototype.contains = function (needle) {
+	for (key in this) {
+		if (this[key] === needle) {
+			return true;
+		}
+	}
+	return false;
+};
+
+
+/**
+	Events:
+	Cross-browser, no-jQuery
+*/
+
+function onEvent(element, type, method) {
+	if (typeof window.addEventListener === "function") {
+		return element.addEventListener(type, method, false);
+	} else if (typeof window.attachEvent === "object") {
+		return element.attachEvent('on' + type, method);
+	}
+} // end onEvent
+
+function offEvent(element, type, method) {
+	if (typeof window.removeEventListener === "function") {
+		return element.removeEventListener(type, method, false);
+	} else if (typeof window.detachEvent === "object") {
+		return element.detachEvent('on' + type, method);
+	}
+} // end offEvent
+
+function doEvent(element, type) {
+	if (typeof window.dispatchEvent === "function") {
+		var theEvent = document.createEvent('Event');
+		theEvent.initEvent(type, true, true);
+		element.dispatchEvent(theEvent);
+	} else if (typeof element.fireEvent === "object") {
+		return element.fireEvent('on' + type);
+	}
+} // end doEvent
+
+
+/**
+	Cookies:
+	http://www.quirksmode.org/js/cookies.html
+*/
+
+function setCookie(name, value, days, path) {
+	var date, expires;
+	if (days) {
+		date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toGMTString();
+	} else {
+		expires = "";
+	}
+	if(path) {
+		path = "; path=" + path;
+	} else {
+		path = "; path=/";
+	}
+	document.cookie = name + "=" + value + expires + path;
+} // end setCookie
+
+function getCookie(name) {
+	var nameEQ = name + "=",
+		ca = document.cookie.split(';');
+	for (var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+} // end getCookie
+
+function unsetCookie(name, path) {
+	setCookie(name, "", -1, path);
+} // end unsetCookie
 
