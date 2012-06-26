4ac2e1bd6cbbeaa6c480d79c2eb8a6abd09096da
fabrique <fabrique@dev.fabriquehq.nl>
2012-06-26 11:54:07 +0200

    Forgot these.

--- 1.4/website/urls.py
+++ 1.4/website/urls.py
@@ -1,11 +1,12 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-
     # Examples:
     url(r'^$', 'website.views.home', name='home'),
     #
@@ -15,3 +16,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
--- 1.4/website/views.py
+++ 1.4/website/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, 'homepage.html', {})
